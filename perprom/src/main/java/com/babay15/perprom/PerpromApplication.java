b0bb2be4b8cebd0ca57f71065fc55ddad408cd43
babaymxc <babaymxc@gmail.com>
2020-01-12 03:16:13 +0700

    Initial commit

---
+++ perprom/src/main/java/com/babay15/perprom/PerpromApplication.java
@@ -0,0 +1,13 @@
+package com.babay15.perprom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerpromApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerpromApplication.class, args);
+    }
+
+}
---
+++ perprom/src/test/java/com/babay15/perprom/PerpromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babay15.perprom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerpromApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
