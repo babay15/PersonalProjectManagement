dc2a389e4a932d09fe3214ae916c657fb9baa750
babaymxc <babaymxc@gmail.com>
2020-01-12 05:48:44 +0700

    Adding a custom exception

--- perprom/src/main/java/com/babay15/perprom/services/ProjectService.java
+++ perprom/src/main/java/com/babay15/perprom/services/ProjectService.java
@@ -1,5 +1,6 @@
 package com.babay15.perprom.services;
 
+import com.babay15.perprom.Exceptions.ProjectIdException;
 import com.babay15.perprom.domain.Project;
 import com.babay15.perprom.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception e){
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + " already exists");
+        }
     }
 }
